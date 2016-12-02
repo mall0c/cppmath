facaad28c0ae37bf9d503b7ba2cc75505632e51a
Marvin Ewald <marvin.e@protonmail.ch>
2016-12-02 20:29:40 +0100

    Make c++11 required

--- include/math/compat.hpp
+++ include/math/compat.hpp
@@ -1,8 +1,8 @@
 #ifndef CPPMATH_CPP11_COMPAT_HPP
 #define CPPMATH_CPP11_COMPAT_HPP
 
-// Disable constexpr if not compiled with c++11 or incompatible Microsoft compiler is used.
-#if (defined(__GNUC__) && __cplusplus < 201103) || (defined(_WIN32) && _MSC_VER<1900)
+// Disable constexpr when compiling with an incompatible Microsoft compiler.
+#if (defined(_WIN32) && _MSC_VER<1900)
 #define constexpr
 #endif
 
